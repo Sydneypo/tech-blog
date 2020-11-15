eacc102dd76047aac13a737e9fc88e9ce8a3ba67
Murphy <sydneyporter74@yahoo.com>
2020-11-15 16:34:40 -0500

    completed routes

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,12 +1,20 @@
 const router = require('express').Router();
-const { Post, User } = require('../../models');
+const { Post, User, Comment } = require('../../models');
 
 // get all users
 router.get('/', (req, res) => {
     Post.findAll({
-        attributes: ['id', 'post_url', 'title', 'created_at'],
         order: [['created_at', 'DESC']],
+        attributes: ['id', 'post_url', 'title', 'created_at'],
         include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },    
             {
                 model: User,
                 attributes: ['username']
@@ -29,9 +37,13 @@ router.get('/:id', (req, res) => {
         attributes: ['id', 'post_url', 'title', 'created_at'],
         include: [
             {
-                model: User,
-                attributes: ['username']
-            }
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
         ]
     })
         .then(dbPostData => {
